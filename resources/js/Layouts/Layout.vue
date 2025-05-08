4e0ddeca327bf35cc8946a98fca0f512d6b37d69
beybora <bora.b.sarikaya@gmail.com>
2025-05-08 21:26:34 +0200

    create login page and add it to layout

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -7,6 +7,7 @@
                     <Link :href="route('home')">Home</Link>
                     <Link :href="route('about')">About</Link>
                     <Link :href="route('register')">Register</Link>
+                    <Link :href="route('login')">Login</Link>
                 </div>
             </nav>
         </header>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,19 +1,13 @@
 <template>
-    <Head title="Register"></Head>
+    <Head title="Login"></Head>
 
     <div class="min-h-screen flex flex-col justify-center items-center">
-        <h1 class="text-3xl font-bold mb-8 text-gray-800">Register</h1>
+        <h1 class="text-3xl font-bold mb-8 text-gray-800">
+            Login to your account
+        </h1>
 
         <div class="w-full max-w-lg bg-white p-8 rounded shadow-md">
             <form @submit.prevent="submit">
-                <TextInput
-                    name="name"
-                    type="text"
-                    v-model="form.name"
-                    :message="form.errors.name"
-                    placeholder="Enter your name"
-                ></TextInput>
-
                 <TextInput
                     name="email"
                     type="text"
@@ -29,27 +23,18 @@
                     :message="form.errors.password"
                     placeholder="Enter your password"
                 ></TextInput>
-
-                <TextInput
-                    name="confirm password"
-                    type="password"
-                    v-model="form.password_confirmation"
-                    :message="form.errors.password_confirmation"
-                    placeholder="Confirm your password"
-                ></TextInput>
-
                 <button
                     type="submit"
                     :disabled="form.processing"
                     class="w-full bg-blue-600 text-white p-3 rounded font-semibold hover:bg-blue-700 transition"
                 >
-                    Register
+                    Login
                 </button>
 
                 <p class="text-center text-slate-600 mt-4">
-                    Already a user?
+                    Need an account?
                     <a
-                        :href="route('login')"
+                        :href="route('register')"
                         class="text-blue-500 hover:underline"
                         >Login</a
                     >
@@ -64,19 +49,14 @@ import { useForm } from "@inertiajs/vue3";
 import TextInput from "@/Components/TextInput.vue";
 
 const form = useForm({
-    name: null,
     email: null,
     password: null,
-    password_confirmation: null,
 });
 
 const submit = () => {
-    form.post(route("register"), {
+    form.post(route("login"), {
         onError: () => {
-            form.reset("password", "password_confirmation");
-        },
-        onFinish: () => {
-            form.reset("password", "password_confirmation");
+            form.reset("password");
         },
     });
 };
