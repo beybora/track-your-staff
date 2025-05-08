e64798dc2028979137699b47b40979bb746aae5a
beybora <bora.b.sarikaya@gmail.com>
2025-05-08 23:17:05 +0200

    add dynamic auth links for logged in and logged out users

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
-        <header class="bg-cyan-700 text-white">
-            <nav class="flex items-center justify-between p-4 max-w-screen-2xl">
+        <header class="bg-cyan-700">
+            <nav
+                class="flex justify-between items-center p-4 max-w-screen-2xl mx-auto"
+            >
                 <div class="space-x-6">
-                    <h1>{{ title }}</h1>
                     <Link :href="route('home')">Home</Link>
                     <Link :href="route('about')">About</Link>
-                    <Link :href="route('register')">Register</Link>
-                    <Link :href="route('login')">Login</Link>
+                </div>
+
+                <div class="space-x-6">
+                    <div
+                        class="space-x-6"
+                        v-if="$page.props.auth && $page.props.auth.user"
+                    >
+                        <Link :href="route('dashboard')">Dashboard</Link>
+                        <Link
+                            :href="route('logout')"
+                            method="post"
+                            as="button"
+                            class="text-white bg-red-400 px-4 py-2 rounded"
+                        >
+                            Logout
+                        </Link>
+                        <span class="text-xl">{{
+                            $page.props.auth.user.name
+                        }}</span>
+                    </div>
+                    <div v-else class="space-x-6">
+                        <Link :href="route('register')">Register</Link>
+                        <Link :href="route('login')">Login</Link>
+                    </div>
                 </div>
             </nav>
         </header>
