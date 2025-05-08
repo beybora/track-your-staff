06669192bea2867a31c095d16e200f7272404bba
beybora <bora.b.sarikaya@gmail.com>
2025-05-08 13:02:40 +0200

    set default layout for Inertia pages, add page title for Inertia pages, create Layout.vue and wrap Home with Layout

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header class="bg-cyan-700 text-white">
+            <nav class="flex items-center justify-between p-4 max-w-screen-2xl">
+                <div class="space-x-6">
+                    <h1>{{ title }}</h1>
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                </div>
+            </nav>
+        </header>
+        <main class="p-4 max-w-screen-2xl">
+            <slot></slot>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import { Link, Head } from "@inertiajs/vue3";
+</script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="container">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is a simple Vue.js application.</p>
-    </div>
+    <Head>
+        <title>Home</title>
+    </Head>
+    <h1>Welcome to the Home Page</h1>
 </template>
+
+<script setup>
+import { Link, Head } from "@inertiajs/vue3";
+</script>
