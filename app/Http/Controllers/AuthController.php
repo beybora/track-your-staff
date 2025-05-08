920cf0580bf6b3968404b5c4efee040e3c610082
beybora <bora.b.sarikaya@gmail.com>
2025-05-08 20:04:12 +0200

    create auth controller with basic validation

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+
+        $user = User::create($fields);
+
+        //Auth::login($user);
+
+        return redirect()->route('home');
+    }
+}
